28d358a65e0994af8595fa01ba3a6df99313fe95
kali-tan <tank4@vcu.edu>
2025-02-19 22:35:18 -0500

    second round
    curosr works again!

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $( function() {
     $(".draggable" ).draggable();
 } );
+
+$(".smiski").click(
+    function(){
+        $(".one").toggleClass("visible");
+    }
+)
+
+$(".stickers").click(
+    function(){
+        $(".two").toggleClass("visible");
+    }
+)
+
